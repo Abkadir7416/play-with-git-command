97b23e68d9daebc25cc9cb20bd8705eb1d068054
Abdul Kadir (Technology - New Delhi) <abdul.kadir@partner.htmedialabs.com>
2025-05-24 15:50:49 +0530

    first commit

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.get('/', (req, res) => {
 });
 
 
+app.get('/first', (req, res) => {
+    res.json({ message: 'first commit' });
+  });
+  
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
